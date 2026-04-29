b9c2f99d899a809766de6beaeeb19ce0e535a308
rodrigomes002 <rodrigomes002@gmail.com>
2026-04-29 10:14:17 -0300

    style: comment

--- Core/Entities/Policy.cs
+++ Core/Entities/Policy.cs
@@ -6,6 +6,6 @@ public class Policy
 {
     public Guid Id { get; set; }
     public PolicyType Type { get; set; }
-    public string Target { get; set; } // domínio ou processo
+    public string Target { get; set; }
     public bool Enabled { get; set; }
 }
